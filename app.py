ada472ae2f2a15e588bde58b1e7cb136e8bc191f
okovsky <olegkovskyy@gmail.com>
2023-10-08 16:46:04 +0300

    Added image obrabotka

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import io
 import json
+import PIL
 
-from flask import Flask, send_from_directory, render_template, url_for, request
-import os
+from PIL import Image
+from PIL.ImageOps import crop
+from flask import Flask, send_from_directory, render_template, request
 
 app = Flask(__name__)
 
@@ -33,13 +36,35 @@ def upload_file():
 
     file = request.files['newImage']
 
+    image = Image.open(file)
+    image_width = image.size[0]
+    image_height = image.size[1]
+
+    if image_width == image_height:
+        new_image = image.resize((142, 142))
+    elif image_width > image_height:
+        new_image = image.resize((round(image_width / image_height * 142), 142))
+        # обрезать ширину
+        temp_size = (round(image_width / image_height * 142) - 142) / 2
+        new_image = new_image.crop((temp_size, 0, temp_size + 142, 142))
+    else:
+        new_image = image.resize((142, round(image_height / image_width * 142)))
+        # обрезать высоту
+        temp_size = (round(image_height / image_width * 142) - 142) / 2
+        new_image = new_image.crop((0, temp_size, 142, temp_size + 142))
+
     if file.filename == '':
         return False
 
-    if file:
+    if image:
         filename = f'./static/photos/{file.filename}'
-        file.save(filename)
+        new_image.save(filename)
         return filename
+
+    # if file:
+    #     filename = f'./static/photos/{file.filename}'
+    #     file.save(filename)
+    #     return filename
 
 if __name__ == '__main__':
     app.run()
